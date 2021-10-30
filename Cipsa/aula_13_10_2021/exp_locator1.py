19dd7be54dd46e430d249412f2b5d45ed2cd71e3
DenisPinto <63479548+DenisPinto@users.noreply.github.com>
2021-10-30 19:20:05 +0200

    Update exp_locator1.py

--- Cipsa/aula_13_10_2021/exp_locator1.py
+++ Cipsa/aula_13_10_2021/exp_locator1.py
@@ -1,6 +1,7 @@
+!pip install geopy
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="geocipsa")
-cities = ['Barcelona','Madrid','London','Paris','Belem','Israel','Belém','Macapá']
+cities = ['Barcelona','Madrid','London','Paris']
 for c in cities:
   coord = geolocator.geocode (c)
   print(coord.latitude, coord.longitude, c)
